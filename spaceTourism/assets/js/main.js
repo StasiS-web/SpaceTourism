1e4f224b2069f17938b89f469e8620d43363ee85
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2023-02-06 00:40:10 +0000

    open/close functionality for mobile nav

---
+++ spaceTourism/assets/js/main.js
@@ -0,0 +1,16 @@
+const toggleBtn = document.querySelector('.mobile-nav-toggle');
+const nav = document.querySelector('.primary-navigation');
+
+
+toggleBtn.addEventListener('click', () => {
+    const visibility = nav.getAttribute("data-visible");
+
+    if (visibility === 'false') {
+        nav.setAttribute('data-visible', true);
+        toggleBtn.setAttribute('aria-expanded', true);
+    }
+    else{
+        nav.setAttribute('data-visible', false);
+        toggleBtn.setAttribute('aria-expanded', false);
+    }
+});
